31d904d0dbc54491c1d15e0613630f65ebf0e978
Evgeny Tabatsky <e.tabatsky@gmail.com>
2022-03-08 13:50:14 +1000

    bug with toasts fixed.

--- commons/src/main/java/jatx/extensions/Context.kt
+++ commons/src/main/java/jatx/extensions/Context.kt
@@ -1,11 +1,23 @@
 package jatx.extensions
 
 import android.content.Context
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.StringRes
+import jatx.debug.R
+
 
 fun Context.showToast(text: String) {
-    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    val toast = Toast.makeText(this, text, Toast.LENGTH_LONG)
+    val view = toast.view
+
+    view?.setBackgroundResource(R.drawable.background_toast)
+
+    val text = view?.findViewById(android.R.id.message) as TextView?
+    text?.setBackgroundColor(resources.getColor(R.color.gray_aa))
+    text?.setTextColor(resources.getColor(R.color.black))
+
+    toast.show()
 }
 
 fun Context.showToast(@StringRes resId: Int) {
